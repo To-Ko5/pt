4932222aaedfdf21de617a880ba8f844186671c5
to <a>
2020-01-20 14:50:49 +0900

    modified html & header create

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,55 +1,53 @@
 <template>
   <div>
+    <headerSection/>
     <nuxt />
   </div>
 </template>
 
-<style>
+
+<script>
+import header from '~/components/header.vue'
+export default {
+  components: {
+    headerSection: header
+  }
+}
+</script>
+
+<style lang="scss">
+html,body,h1,h2,h3,ul,li {
+   margin: 0;
+   padding: 0;
+   line-height: 1.0;
+}
+
+ul {
+    list-style: none;
+}
+a {
+    text-decoration: none;
+}
+
+table {
+border-collapse: collapse;
+border-spacing: 0;
+}
+
+img {
+    border: none;
+    vertical-align: bottom;
+    max-width: 100%;
+    height: auto;
+}
+/* reset */
 html {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
+    font-size: 62.5%;
 }
 
-*,
-*:before,
-*:after {
-  box-sizing: border-box;
-  margin: 0;
-}
-
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
+body {
+   font-size: 1.6rem;
+   color: #333;
+   font-weight: 500;
 }
 </style>
